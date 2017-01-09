2973457f61edb85f136fa3513a7b57e5835bf76d
J.C. v. Weert <justinvw68@hotmail.com>
2017-01-09 10:17:06 +0100

    initial commit

---
+++ linux_standard.py
@@ -0,0 +1,10 @@
+def install():
+	print("sudo apt-get dist-upgrade")
+	print("sudo apt-get update")
+	print("sudo apt-get install build-essentials")
+	print("sudo apt-get install git-core")
+	print("git init")
+	print("git config --global user.mail 'git-email'")
+	print("git config --global user.name 'Real-Name'")
+
+install()
